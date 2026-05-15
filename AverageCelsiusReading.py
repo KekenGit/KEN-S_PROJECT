ca923053feac0599e1233bf04479c842d34bda28
KekenGit <jukentubal-it@srcb.edu.ph>
2026-05-15 18:37:21 +0800

    Update AverageCelsiusReading.py

--- AverageCelsiusReading.py
+++ AverageCelsiusReading.py
@@ -1,5 +1,31 @@
-temps = list(map(float, input("Enter temperatures: ").split()))
+# Accepts how many temperature readings to enter
+count = int(input("How many Celsius readings? "))
 
-average = sum(temps) / len(temps)
+# Creates an empty list
+temperatures = []
 
-print("Average:", average)
+# Loops based on user input
+for i in range(count):
+
+    # Accepts each temperature
+    temp = float(input(f"Enter reading #{i + 1}: "))
+
+    # Adds temperature to the list
+    temperatures.append(temp)
+
+# Calculates average
+average = sum(temperatures) / len(temperatures)
+
+# Displays all readings
+print("\nTemperature Readings:", temperatures)
+
+# Displays the average
+print("Average Celsius Reading:", round(average, 2))
+
+# Checks temperature condition
+if average >= 35:
+    print("Weather Status: Very Hot")
+elif average >= 25:
+    print("Weather Status: Warm")
+else:
+    print("Weather Status: Cold")
